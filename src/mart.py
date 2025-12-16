a6eba1fecfe2f9501ef2a890712667a31c3a05ba
nhatquang-nguyen-129 <nhatquang.nguyen.129@gmail.com>
2025-12-16 19:35:57 +0700

    add ICT timezone

--- src/mart.py
+++ src/mart.py
@@ -27,11 +27,17 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
 
+# Add Python datetime utilities for integration
+from datetime import datetime
+
 # Add Python logging ultilities for integraton
 import logging
 
 # Add Python time ultilities for integration
 import time
+
+# Add Python IANA time zone ultilities for integration
+from zoneinfo import ZoneInfo
 
 # Add Google Cloud modules for integration
 from google.cloud import bigquery
@@ -65,18 +71,19 @@ def mart_campaign_all() -> dict:
     logging.info(f"🚀 [MART] Starting to build materialized table Facebook Ads campaign performance...")
 
     # 1.1.1. Start timing the Facebook Ads campaign materialization
+    ICT = ZoneInfo("Asia/Ho_Chi_Minh")    
     mart_time_start = time.time()
     mart_sections_status = {}
     mart_sections_time = {}
-    print(f"🔍 [MART] Proceeding to build materialized table for Facebook Ads campaign performance at {time.strftime('%Y-%m-%d %H:%M:%S')}...")
-    logging.info(f"🔍 [MART] Proceeding to build materialized table for Facebook Ads campaign performance at {time.strftime('%Y-%m-%d %H:%M:%S')}...")
+    print(f"🔍 [MART] Proceeding to build materialized table for Facebook Ads campaign performance at {datetime.now(ICT).strftime("%Y-%m-%d %H:%M:%S")}...")
+    logging.info(f"🔍 [MART] Proceeding to build materialized table for Facebook Ads campaign performance at {datetime.now(ICT).strftime("%Y-%m-%d %H:%M:%S")}...")
 
     try:
 
     # 1.1.2. Prepare Google BigQuery table_id for materialization
+        mart_section_name = "[MART] Prepare Google BigQuery table_id for materialization"
+        mart_section_start = time.time()        
         try: 
-            mart_section_name = "[MART] Prepare Google BigQuery table_id for materialization"
-            mart_section_start = time.time()
             staging_dataset = f"{COMPANY}_dataset_{PLATFORM}_api_staging"
             staging_table_campaign = f"{PROJECT}.{staging_dataset}.{COMPANY}_table_{PLATFORM}_all_all_campaign_insights"
             print(f"🔍 [MART] Using staging table {staging_table_campaign} to build materialized table for Facebook Ads campaign performance...")
@@ -97,9 +104,9 @@ def mart_campaign_all() -> dict:
             print(f"🔍 [MART] Initializing Google BigQuery client for Google Cloud Platform project {PROJECT}...")
             logging.info(f"🔍 [MART] Initializing Google BigQuery client for Google Cloud Platform project {PROJECT}...")
             google_bigquery_client = bigquery.Client(project=PROJECT)
-            print(f"✅ [MART] Successfully initialized Google BigQuery client for Google Cloud Platform project {PROJECT}.")
-            logging.info(f"✅ [MART] Successfully initialized Google BigQuery client for Google Cloud Platform project {PROJECT}.")
             mart_sections_status[mart_section_name] = "succeed"
+            print(f"✅ [MART] Successfully initialized Google BigQuery client for Google Cloud Platform project {PROJECT}.")
+            logging.info(f"✅ [MART] Successfully initialized Google BigQuery client for Google Cloud Platform project {PROJECT}.")            
         except Exception as e:
             mart_sections_status[mart_section_name] = "failed"
             print(f"❌ [MART] Failed to initialize Google BigQuery client for Google Cloud Platform project {PROJECT} due to {e}.")
@@ -111,56 +118,59 @@ def mart_campaign_all() -> dict:
         mart_section_name = "[MART] Query staging table for materialization"
         mart_section_start = time.time()    
         try:
-            query = f"""
-            CREATE OR REPLACE TABLE `{mart_table_all}`
-            PARTITION BY ngay
-            CLUSTER BY nhan_su, ma_ngan_sach_cap_1, nganh_hang, hang_muc
-            AS
-            WITH base AS (
-                SELECT
-                    SAFE_CAST(enrich_account_name AS STRING) AS tai_khoan,
-                    SAFE_CAST(enrich_account_department AS STRING) AS phong_ban,
-                    SAFE_CAST(enrich_account_platform AS STRING) AS nen_tang,
-                    SAFE_CAST(enrich_budget_group AS STRING) AS ma_ngan_sach_cap_1,
-                    SAFE_CAST(enrich_budget_type AS STRING) AS ma_ngan_sach_cap_2,
-                    SAFE_CAST(enrich_program_track AS STRING) AS hang_muc,
-                    SAFE_CAST(enrich_program_group AS STRING) AS chuong_trinh,
-                    SAFE_CAST(enrich_program_type AS STRING) AS noi_dung,
-                    SAFE_CAST(enrich_campaign_objective AS STRING) AS hinh_thuc,
-                    SAFE_CAST(enrich_campaign_region AS STRING) AS khu_vuc,
-                    SAFE_CAST(enrich_campaign_personnel AS STRING) AS nhan_su,
-                    SAFE_CAST(enrich_category_group AS STRING) AS nganh_hang,
-                    SAFE_CAST(campaign_name AS STRING) AS campaign_name,
-                    CAST(date AS DATE) AS ngay,
-                    SAFE_CAST(year AS STRING) AS nam,
-                    SAFE_CAST(month AS STRING) AS thang,
-                    SAFE_CAST(spend AS FLOAT64) AS spend,
-                    SAFE_CAST(result AS FLOAT64) AS result,
-                    SAFE_CAST(result_type AS STRING) AS result_type,
-                    SAFE_CAST(purchase AS FLOAT64) AS purchase,
-                    SAFE_CAST(messaging_conversations_started AS FLOAT64) AS message,
-                    SAFE_CAST(impressions AS INT64) AS impression,
-                    SAFE_CAST(clicks AS INT64) AS click,
-                    CASE
-                        WHEN REGEXP_CONTAINS(delivery_status, r"ACTIVE") THEN "🟢"
-                        WHEN REGEXP_CONTAINS(delivery_status, r"PAUSED") THEN "⚪"
-                        ELSE "❓ Unrecognized"
-                    END AS trang_thai,
-                    SAFE_CAST(last_updated_at AS TIMESTAMP) AS last_updated_at
-                FROM `{staging_table_campaign}`
-                WHERE date IS NOT NULL
-            )
-            SELECT *
-            FROM base;
+            query_replace_config = f"""
+                CREATE OR REPLACE TABLE `{mart_table_all}`
+                PARTITION BY ngay
+                CLUSTER BY nhan_su, ma_ngan_sach_cap_1, nganh_hang, hang_muc
+                AS
+                WITH base AS (
+                    SELECT
+                        SAFE_CAST(enrich_account_name AS STRING) AS tai_khoan,
+                        SAFE_CAST(enrich_account_department AS STRING) AS phong_ban,
+                        SAFE_CAST(enrich_account_platform AS STRING) AS nen_tang,
+                        SAFE_CAST(enrich_budget_group AS STRING) AS ma_ngan_sach_cap_1,
+                        SAFE_CAST(enrich_budget_type AS STRING) AS ma_ngan_sach_cap_2,
+                        SAFE_CAST(enrich_program_track AS STRING) AS hang_muc,
+                        SAFE_CAST(enrich_program_group AS STRING) AS chuong_trinh,
+                        SAFE_CAST(enrich_program_type AS STRING) AS noi_dung,
+                        SAFE_CAST(enrich_campaign_objective AS STRING) AS hinh_thuc,
+                        SAFE_CAST(enrich_campaign_region AS STRING) AS khu_vuc,
+                        SAFE_CAST(enrich_campaign_personnel AS STRING) AS nhan_su,
+                        SAFE_CAST(enrich_category_group AS STRING) AS nganh_hang,
+                        SAFE_CAST(campaign_name AS STRING) AS campaign_name,
+                        CAST(date AS DATE) AS ngay,
+                        SAFE_CAST(year AS STRING) AS nam,
+                        SAFE_CAST(month AS STRING) AS thang,
+                        SAFE_CAST(spend AS FLOAT64) AS spend,
+                        SAFE_CAST(result AS FLOAT64) AS result,
+                        SAFE_CAST(result_type AS STRING) AS result_type,
+                        SAFE_CAST(purchase AS FLOAT64) AS purchase,
+                        SAFE_CAST(messaging_conversations_started AS FLOAT64) AS message,
+                        SAFE_CAST(impressions AS INT64) AS impression,
+                        SAFE_CAST(clicks AS INT64) AS click,
+                        CASE
+                            WHEN REGEXP_CONTAINS(delivery_status, r"ACTIVE") THEN "🟢"
+                            WHEN REGEXP_CONTAINS(delivery_status, r"PAUSED") THEN "⚪"
+                            ELSE "❓ Unrecognized"
+                        END AS trang_thai,
+                        SAFE_CAST(last_updated_at AS TIMESTAMP) AS last_updated_at
+                    FROM `{staging_table_campaign}`
+                    WHERE date IS NOT NULL
+                )
+                SELECT *
+                FROM base;
             """
             print(f"🔄 [MART] Querying staging Facebook Ads campaign insights table {staging_table_campaign} to create or replace materialized table for campaign performance...")
             logging.info(f"🔄 [MART] Querying staging Facebook Ads campaign insights table {staging_table_campaign} to create or replace materialized table for campaign performance...")
-            google_bigquery_client.query(query).result()
-            count_query = f"SELECT COUNT(1) AS row_count FROM `{mart_table_all}`"
-            row_count = list(google_bigquery_client.query(count_query).result())[0]["row_count"]
-            print(f"✅ [MART] Successfully created or replace materialized table {mart_table_all} for Facebook Ads campaign performance with {row_count} row(s).")
-            logging.info(f"✅ [MART] Successfully created or replace materialized table {mart_table_all} for Facebook Ads campaign performance with {row_count} row(s).")
+            query_replace_load = google_bigquery_client.query(query_replace_config)
+            query_replace_result = query_replace_load.result()
+            query_count_config = f"SELECT COUNT(1) AS mart_rows_count FROM `{mart_table_all}`"
+            query_count_load = google_bigquery_client.query(query_count_config)
+            query_count_result = query_count_load.result()
+            mart_rows_uploaded = list(query_count_result)[0]["mart_rows_count"]
             mart_sections_status[mart_section_name] = "succeed"
+            print(f"✅ [MART] Successfully created or replace materialized table {mart_table_all} for Facebook Ads campaign performance with {mart_rows_uploaded} row(s).")
+            logging.info(f"✅ [MART] Successfully created or replace materialized table {mart_table_all} for Facebook Ads campaign performance with {mart_rows_uploaded} row(s).")            
         except Exception as e:
             mart_sections_status[mart_section_name] = "failed"
             print(f"❌ [MART] Failed to create or replace materialized table for Facebook Ads campaign performance due to {e}.")
@@ -174,25 +184,26 @@ def mart_campaign_all() -> dict:
         mart_sections_total = len(mart_sections_status) 
         mart_sections_failed = [k for k, v in mart_sections_status.items() if v == "failed"] 
         mart_sections_succeeded = [k for k, v in mart_sections_status.items() if v == "succeed"]
+        mart_rows_output = mart_rows_uploaded
         mart_sections_summary = list(dict.fromkeys(
             list(mart_sections_status.keys()) +
             list(mart_sections_time.keys())
-        ))
+            ))
         mart_sections_detail = {
             mart_section_summary: {
                 "status": mart_sections_status.get(mart_section_summary, "unknown"),
                 "time": round(mart_sections_time.get(mart_section_summary, 0.0), 2),
-            }
+                }
             for mart_section_summary in mart_sections_summary
-        }       
-        if len(mart_sections_failed) > 0:
-            print(f"❌ [MART] Failed to complete Facebook Ads campaign performance materialization due to unsuccessful section(s) {', '.join(mart_sections_failed)}.")
-            logging.error(f"❌ [MART] Failed to complete Facebook Ads campaign performance materialization due to unsuccessful section(s) {', '.join(mart_sections_failed)}.")
+            }       
+        if mart_sections_failed:
             mart_status_final = "mart_failed_all"
+            print(f"❌ [MART] Failed to complete Facebook Ads campaign performance materialization with {mart_rows_output} materialized row(s) due to {', '.join(mart_sections_failed)} failed section(s) in {mart_time_elapsed}s.")
+            logging.error(f"❌ [MART] Failed to complete Facebook Ads campaign performance materialization with {mart_rows_output} materialized row(s) due to {', '.join(mart_sections_failed)} failed section(s) in {mart_time_elapsed}s.")
         else:
-            print(f"🏆 [MART] Successfully completed Facebook Ads campaign performance materialization in {mart_time_elapsed}s.")
-            logging.info(f"🏆 [MART] Successfully completed Facebook Ads campaign performance materialization in {mart_time_elapsed}s.")
             mart_status_final = "mart_succeed_all"
+            print(f"🏆 [MART] Successfully completed Facebook Ads campaign performance materialization with {mart_rows_output} materialized row(s) in {mart_time_elapsed}s.")
+            logging.info(f"🏆 [MART] Successfully completed Facebook Ads campaign performance materialization with {mart_rows_output} materialized row(s) in {mart_time_elapsed}s.")
         mart_results_final = {
             "mart_df_final": None,
             "mart_status_final": mart_status_final,
@@ -202,8 +213,9 @@ def mart_campaign_all() -> dict:
                 "mart_sections_succeed": mart_sections_succeeded,
                 "mart_sections_failed": mart_sections_failed,
                 "mart_sections_detail": mart_sections_detail,
-            },
-        }
+                "mart_rows_output": mart_rows_output,
+                },
+            }
     return mart_results_final
 
 # 2. BUILD MONTHLY MATERIALIZED TABLE FOR CREATIVE PERFORMANCE FROM STAGING TABLES
