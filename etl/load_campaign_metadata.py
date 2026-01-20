7bc3ab22805ad37c51abb96710be3a26f775c86a
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-01-20 11:13:54 +0700

    chagne to campaign_id based deduplication

---
+++ etl/load_campaign_metadata.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import logging
+import pandas as pd
+from plugins.google_bigquery import GoogleBigqueryLoader
+sys.path.append(
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__), "../../"
+        )
+    )
+)
+
+def load_campaign_metadata(
+    *,
+    df: pd.DataFrame,
+    direction: str,
+) -> None:
+    """
+    Facebook Ads campaign metadata loader
+    ----------------------
+    Workflow:
+        1. Validate input DataFrame
+            Empty input DataFrame triggers 'return'
+        2. Log loading metadata
+            Row count, destination table, partition, cluster...
+        3. Trigger GoogleBigqueryLoader
+           UPSERT mode, campaign_id-based deduplication
+    ---------
+    Parameters:
+        1. df: pd.DataFrame 
+            Facebook Ads campaign metadata flattended DataFrame
+        2. direction: str
+            Must be 'project.dataset.table' for Google BigQuery
+    ---------
+    Returns:
+        1. None
+            This function performs data loading as a side effect and does not return any value.
+    """    
+
+    if df.empty:
+        msg = ("⚠️ [LOADER] Empty Facebook Ads campaign metadata Dataframe then loading will be skipped.")
+        print(msg)
+        logging.warning(msg)
+        return
+
+    msg = (
+        "🔄 [LOADER] Triggering to load "
+        f"{len(df)} row(s) of Facebook Ads campaign metadata to Google BigQuery table "
+        f"{direction}..."
+        )
+    GoogleBigqueryLoader.load(
+        df=df,
+        direction=direction,
+        mode="upsert",
+        keys=[
+            "account_id", 
+            "campaign_id"
+            ],
+        partition=None,
+        cluster=None,
+    )
