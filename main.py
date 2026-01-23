bc09751716f925280fa36152263006a5805faf51
nhatquang-nguyen-129 <nhatquang.nguyen.129@gmail.com>
2026-01-23 22:42:17 +0700

    use ICT time zone

--- main.py
+++ main.py
@@ -1,126 +1,237 @@
-"""
-==================================================================
-FACEBOOK MAIN ENTRYPOINT
-------------------------------------------------------------------
-This script serves as the unified CLI controller for triggering  
-ads data updates from Facebook Ads based on command-line arguments 
-and environment variables.
-
-It supports incremental daily ingestion for selected data layers  
-(e.g. campaign, ad) and allows flexible control over date ranges.
-
-✔️ Dynamic routing to the correct update module based on PLATFORM  
-✔️ CLI flags to select data layers and date range mode  
-✔️ Shared logging and error handling across update jobs  
-✔️ Supports scheduled jobs or manual on-demand executions 
-✔️ Automatic update summary and execution timing for each step 
-
-⚠️ This script does *not* contain data processing logic itself.  
-It simply delegates update tasks to platform-specific modules .
-==================================================================
-"""
-
-# Add project root to sys.path to enable absolute imports
 import os
+from pathlib import Path
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+ROOT_FOLDER_LOCATION = Path(__file__).resolve().parents[0]
+sys.path.append(str(ROOT_FOLDER_LOCATION))
 
-# Add Python datetime utilities for integration
-from datetime import (
-    datetime, 
-    timedelta
-)
-
-# Add Python dynamic import ultilities for integration
-import importlib
-
-# Add Python logging ultilities for integration
+from datetime import datetime, timedelta
+import json
 import logging
+from zoneinfo import ZoneInfo
 
-# Get environment variable for Company
-COMPANY = os.getenv("COMPANY") 
+from google.ads.googleads.client import GoogleAdsClient
+from google.cloud import secretmanager
+from google.api_core.client_options import ClientOptions
 
-# Get environment variable for Google Cloud Project ID
+from dags.dags_facebook_ads import dags_facebook_ads
+
+COMPANY = os.getenv("COMPANY")
 PROJECT = os.getenv("PROJECT")
-
-# Get environment variable for Platform
-PLATFORM = os.getenv("PLATFORM")
-
-# Get environmetn variable for Department
 DEPARTMENT = os.getenv("DEPARTMENT")
-
-# Get environment variable for Account
 ACCOUNT = os.getenv("ACCOUNT")
-
-# Get nvironment variable for Layer
-LAYER = os.getenv("LAYER")
-
-# Get environment variable for Mode
 MODE = os.getenv("MODE")
 
-# Get validated environment variables
-if not all([COMPANY, PLATFORM, ACCOUNT, LAYER, MODE]):
-    raise EnvironmentError("❌ [MAIN] Missing required environment variables COMPANY/PLATFORM/ACCOUNT/LAYER/MODE.")
+if not all([
+    COMPANY,
+    PROJECT,
+    DEPARTMENT,
+    ACCOUNT,
+    MODE
+]):
+    raise EnvironmentError("❌ [MAIN] Failed to execute Google Ads main entrypoint due to missing required environment variables.")
 
-# 1. MAIN ENTRYPOINT CONTROLLER
-
-# 1.1. Validate input for main entrypoint function
-if PLATFORM != "facebook":
-    raise ValueError("❌ [MAIN] Only 'facebook' platform is supported in this script.")
-try:
-    update_module_location = importlib.import_module(f"src.update")
-except ModuleNotFoundError:
-    raise ImportError(f"❌ [MAIN] Platform '{PLATFORM}' is not supported so please ensure src/update.py exists.")
-
-# 1.2. Execution controller
 def main():
-    main_date_today = datetime.today()
-    if PLATFORM == "facebook":
-        try:
-            update_campaign_insights = update_module_location.update_campaign_insights
-            update_ad_insights = update_module_location.update_ad_insights
-        except AttributeError:
-            raise ImportError(f"❌ [MAIN] Failed to locate Facebook Ads update module due to update_campaign_insights and update_ad_insights must be defined.")
-        main_layers_variable = [layer.strip() for layer in LAYER.split(",") if layer.strip()]
-        if len(main_layers_variable) != 1:
-            raise ValueError("⚠️ [MAIN] Only one layer is supported per execution so please run separately for each layer.")
-        if MODE == "today":
-            main_date_start = main_date_end = main_date_today.strftime("%Y-%m-%d")
-        elif MODE == "last3days":
-            main_date_start = (main_date_today - timedelta(days=3)).strftime("%Y-%m-%d")
-            main_date_end = (main_date_today - timedelta(days=1)).strftime("%Y-%m-%d")      
-        elif MODE == "last7days":
-            main_date_start = (main_date_today - timedelta(days=7)).strftime("%Y-%m-%d")
-            main_date_end = (main_date_today - timedelta(days=1)).strftime("%Y-%m-%d")
-        elif MODE == "thismonth":
-            main_date_start = main_date_today.replace(day=1).strftime("%Y-%m-%d")
-            main_date_end = main_date_today.strftime("%Y-%m-%d")
-        elif MODE == "lastmonth":
-            main_date_start = (main_date_today.replace(day=1) - timedelta(days=1)).replace(day=1).strftime("%Y-%m-%d")
-            main_date_end = (main_date_today.replace(day=1) - timedelta(days=1)).strftime("%Y-%m-%d")
-        else:
-            raise ValueError(f"⚠️ [MAIN] Unsupported mode {MODE} for Facebook Ads main entrypoint so please re-check input environment variable.")
-        if "campaign" in main_layers_variable:
-            try:
-                print(f"🚀 [MAIN] Starting to update '{PLATFORM}' campaign performance of '{COMPANY}' company in '{MODE}' mode and '{LAYER}' layer from {main_date_start} to {main_date_end}...")
-                logging.info(f"🚀 [MAIN] Starting to update '{PLATFORM}' campaign performance of '{COMPANY}' company in '{MODE}' mode and '{LAYER}' layer from {main_date_start} to {main_date_end}...")
-                update_campaign_insights(update_date_start=main_date_start, update_date_end=main_date_end)
-            except Exception as e:
-                print(f"❌ [MAIN] Failed to trigger update '{PLATFORM}' campaign insights of '{COMPANY}' in '{MODE}' mode and '{LAYER}' layer from {main_date_start} to {main_date_end} due to {e}.")
-                logging.error(f"❌ [MAIN] Failed to trigger update '{PLATFORM}' campaign insights of '{COMPANY}' in '{MODE}' mode and '{LAYER}' layer from {main_date_start} to {main_date_end} due to {e}.")
-        if "ad" in main_layers_variable:
-            try:
-                print(f"🚀 [MAIN] Starting to update '{PLATFORM}' ad performance of '{COMPANY}' in '{MODE}' mode and '{LAYER}' layer from {main_date_start} to {main_date_end}...")
-                logging.info(f"🚀 [MAIN] Starting to update '{PLATFORM}' ad performance of '{COMPANY}' in '{MODE}' mode and '{LAYER}' layer from {main_date_start} to {main_date_end}...")
-                update_ad_insights(update_date_start=main_date_start, update_date_end=main_date_end)
-            except Exception as e:
-                print(f"❌ [MAIN] Failed to trigger update '{PLATFORM}' ad insights of '{COMPANY}' in '{MODE}' mode and '{LAYER}' layer from {main_date_start} to {main_date_end} due to {e}.")
-                logging.error(f"❌ [MAIN] Failed to trigger update '{PLATFORM}' ad insights of '{COMPANY}' in '{MODE}' mode and '{LAYER}' layer from {main_date_start} to {main_date_end} due to {e}.")
+    """
+    Google Ads entrypoint
+    ---------
+    Main is responsible for preparing the entire execution environment:
+        - Resolve execution time window from MODE
+        - Read & validate OS environment variables
+        - Load secrets from GCP Secret Manager
+        - Initialize Google Ads client exactly once
+        - Dispatch execution to DAG orchestrator
 
-# 1.3. Execute main entrypoint function
+    DAGs must NOT initialize clients or read secrets.
+    DAGs only coordinate execution order and retries.
+    """
+    
+    msg = (
+        "🔄 [MAIN] Triggering to update Google Ads for "
+        f"{ACCOUNT} account of "
+        f"{DEPARTMENT} department in "
+        f"{COMPANY} company with "
+        f"{MODE} mode to Google Cloud project "
+        f"{PROJECT}..."
+    )
+    print(msg)
+    logging.info(msg)    
+
+# Resolve input time range
+    ICT = ZoneInfo("Asia/Ho_Chi_Minh")
+    today = datetime.now(ICT)
+    
+    if MODE == "today":
+        start_date = end_date = today.strftime("%Y-%m-%d")
+
+    elif MODE == "last3days":
+        start_date = (today - timedelta(days=3)).strftime("%Y-%m-%d")
+        end_date = (today - timedelta(days=1)).strftime("%Y-%m-%d")
+
+    elif MODE == "last7days":
+        start_date = (today - timedelta(days=7)).strftime("%Y-%m-%d")
+        end_date = (today - timedelta(days=1)).strftime("%Y-%m-%d")
+
+    elif MODE == "thismonth":
+        start_date = today.replace(day=1).strftime("%Y-%m-%d")
+        end_date = today.strftime("%Y-%m-%d")
+
+    elif MODE == "lastmonth":
+        last_month_end = today.replace(day=1) - timedelta(days=1)
+        start_date = last_month_end.replace(day=1).strftime("%Y-%m-%d")
+        end_date = last_month_end.strftime("%Y-%m-%d")
+
+    else:
+        raise ValueError(f"⚠️ [MAIN] Failed  Unsupported MODE='{MODE}'")
+    
+    msg = (
+        "✅ [MAIN] Successfully resolved "
+        f"{MODE} mode to date range from "
+        f"{start_date} to "
+        f"{end_date}."
+    )
+    print(msg)
+    logging.info(msg)
+
+# Initialize Google Secret Manager
+    try:
+        msg = ("🔍 [MAIN] Initialize Google Secret Manager client...")
+        print(msg)
+        logging.info(msg)
+        
+        google_secret_client = secretmanager.SecretManagerServiceClient(
+            client_options=ClientOptions(
+                api_endpoint="secretmanager.googleapis.com"
+            )
+        )
+
+        msg = ("✅ [MAIN] Successfully initialized Google Secret Manager client.")
+        print(msg)
+        logging.info(msg)
+    
+    except Exception as e:
+        raise RuntimeError(
+            "❌ [MAIN] Failed to initialize Google Secret Manager client due to."
+            f"{e}."
+        )
+        
+# Resolve customer_id from Google Secret Manager
+    try:
+        secret_customer_id = (
+            f"{COMPANY}_secret_{DEPARTMENT}_google_account_id_{ACCOUNT}"
+        )
+        secret_customer_name = (
+            f"projects/{PROJECT}/secrets/{secret_customer_id}/versions/latest"
+        )
+        
+        msg = (
+            "🔍 [MAIN] Retrieving Google Ads secret_customer_id "
+            f"{secret_customer_id} from Google Secret Manager..."
+        )
+        print(msg)
+        logging.info(msg)        
+
+        secret_customer_response = google_secret_client.access_secret_version(
+            name=secret_customer_name,
+            timeout=10.0, #DEBUG
+        )
+        google_customer_id = (
+            secret_customer_response.payload.data.decode("utf-8")
+            .replace("-", "")
+            .replace(" ", "")
+            .strip()
+        )
+        
+        msg = (
+            "✅ [MAIN] Successfully retrieved Google Ads customer_id "
+            f"{google_customer_id} from Google Secret Manager."
+        )
+        print(msg)
+        logging.info(msg)
+    
+    except Exception as e:
+        raise RuntimeError(
+            "❌ [MAIN] Failed to retrieve Google Ads customer_id from Google Secret Manager due to "
+            f"{e}."
+        )
+
+    try:
+        secret_credentials_json = (
+            f"{COMPANY}_secret_all_google_token_access_user"
+        )
+        secret_credentials_name = (
+            f"projects/{PROJECT}/secrets/{secret_credentials_json}/versions/latest"
+        )
+        
+        msg = (
+            "🔍 [MAIN] Retrieving Google Ads secret_credentials_json "
+            f"{secret_credentials_json} from Google Secret Manager..."
+        )
+        print(msg)
+        logging.info(msg)
+
+        secret_credentials_response = google_secret_client.access_secret_version(
+            name=secret_credentials_name
+        )
+        google_ads_credentials = json.loads(
+            secret_credentials_response.payload.data.decode("UTF-8")
+        )
+        
+        msg = ("✅ [MAIN] Successfully retrieved Google Ads credentials from Google Secret Manager.")
+        print(msg)
+        logging.info(msg)
+    
+    except Exception as e:
+        raise RuntimeError(
+            "❌ [MAIN] Failed to retrieve Google Ads credentials from Google Secret Manager due to "
+            f"{e}."
+        )        
+
+# Initialize global Google Ads client
+    google_ads_config = {
+        "developer_token": google_ads_credentials["developer_token"],
+        "client_id": google_ads_credentials["client_id"],
+        "client_secret": google_ads_credentials["client_secret"],
+        "refresh_token": google_ads_credentials["refresh_token"],
+        "login_customer_id": google_ads_credentials["login_customer_id"],
+        "use_proto_plus": True,
+    }
+    try:
+        msg = (
+            "🔍 [MAIN] Initializing global Google Ads client for customer_id "
+            f"{google_customer_id}..."
+        )
+        print(msg)
+        logging.info(msg)
+
+        google_ads_client = GoogleAdsClient.load_from_dict(
+            google_ads_config
+        )
+
+        msg = (
+            "✅ [MAIN] Successfully initialized global Google Ads client for customer_id "
+            f"{google_customer_id}."
+        )
+        print(msg)
+        logging.info(msg)
+    
+    except Exception as e:
+        raise RuntimeError(
+            "❌ [MAIN] Failed to initialize global Google Ads client due to."
+            f"{e}."
+        )
+   
+
+# Execute DAGS
+    dags_facebook_ads(
+        google_ads_client=google_ads_client,
+        customer_id=google_customer_id,
+        start_date=start_date,
+        end_date=end_date
+    )
+
+# Entrypoint
 if __name__ == "__main__":
     try:
         main()
-    except Exception as e:
-        logging.error(f"❌ Update failed: {e}")
+    except Exception:
         sys.exit(1)
