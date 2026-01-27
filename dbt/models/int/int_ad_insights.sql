b6be2a965b842f405737a2c8b0cac47da68a6ab3
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-01-27 14:16:42 +0700

    changed naming convention

---
+++ dbt/models/int/int_ad_insights.sql
@@ -0,0 +1,69 @@
+{{ config(materialized='ephemeral') }}
+
+select
+    date(i.date) as date,
+    
+    insights.account_id,
+    ad.campaign_id,
+    ad.adset_id,
+    insights.ad_id,
+
+    ad.ad_name,
+
+    case
+        when ad.status = 'ACTIVE'                   then '🟢'
+        when ad.status = 'PAUSED'                   then '⚪'
+        when ad.status in ('ARCHIVED', 'DELETED')   then '🔴'
+        else '❓'
+    end as ad_status,
+
+    campaign.campaign_name,
+    campaign.platform,
+    campaign.objective,
+    campaign.budget_group,
+    campaign.region,
+    campaign.category_level_1,
+    campaign.track_group,
+    campaign.pillar_group
+    campaign.content_group
+
+    adset.location,
+    adset.gender,
+    adset.age,
+    adset.audience,
+    adset.format,
+    adset.strategy,
+    adset.type,
+    adset.pillar
+    adset.content
+
+    creative.creative_id,
+    creative.thumbnail_url,
+
+    insights.impressions,
+    insights.clicks,
+    insights.spend,
+
+    insights.result,
+    insights.result_type,
+
+    insights.messaging_conversations_started,
+    insights.purchase
+
+from {{ ref('stg_ad_insights') }} i
+
+left join `{{ target.project }}.{{ var('company') }}_dataset_facebook_api_raw.{{ var('company') }}_table_facebook_{{ var('department') }}_{{ var('account') }}_ad_metadata` a
+    on insights.account_id = ad.account_id
+   and insights.ad_id = ad.ad_id
+
+left join `{{ target.project }}.{{ var('company') }}_dataset_facebook_api_raw.{{ var('company') }}_table_facebook_{{ var('department') }}_{{ var('account') }}_campaign_metadata` cm
+    on insights.account_id = campaign.account_id
+   and insights.campaign_id = campaign.campaign_id
+
+left join `{{ target.project }}.{{ var('company') }}_dataset_facebook_api_raw.{{ var('company') }}_table_facebook_{{ var('department') }}_{{ var('account') }}_adset_metadata` am
+    on insights.account_id = adset.account_id
+   and insights.adset_id = adset.adset_id
+
+left join `{{ target.project }}.{{ var('company') }}_dataset_facebook_api_raw.{{ var('company') }}_table_facebook_{{ var('department') }}_{{ var('account') }}_ad_creative` cr
+    on insights.account_id = creative.account_id
+   and insights.ad_id = creative.ad_id
