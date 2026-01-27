40f788380b4fae93879ff69ea58792230b0345b9
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-01-27 14:20:07 +0700

    changed naming convention

--- dbt/models/int/int_campaign_insights.sql
+++ dbt/models/int/int_campaign_insights.sql
@@ -1,40 +1,40 @@
 {{ config(materialized='ephemeral') }}
 
 select
-    date(i.date) as date,
+    date(insights.date) as date,
 
-    i.account_id,
-    i.campaign_id,
+    insights.account_id,
+    insights.campaign_id,
 
-    m.campaign_name,
+    campaign.campaign_name,
 
     case
-        when m.campaign_status = 'ACTIVE'   then '🟢'
-        when m.campaign_status = 'PAUSED'   then '⚪'
-        when m.campaign_status in ('ARCHIVED', 'DELETED') then '🔴'
+        when campaign.campaign_status = 'ACTIVE'                    then '🟢'
+        when campaign.campaign_status = 'PAUSED'                    then '⚪'
+        when campaign.campaign_status in ('ARCHIVED', 'DELETED')    then '🔴'
         else '❓'
     end as campaign_status,
 
-    m.platform,
-    m.objective,
-    m.budget_group,
-    m.region,
-    m.category_level_1,
-    m.track_group,
-    m.pillar_group,
-    m.content_group,
+    campaign.platform,
+    campaign.objective,
+    campaign.budget_group,
+    campaign.region,
+    campaign.category_level_1,
+    campaign.track_group,
+    campaign.pillar_group,
+    campaign.content_group,
 
-    i.impressions,
-    i.clicks,
-    i.spend,
+    insights.impressions,
+    insights.clicks,
+    insights.spend,
 
-    i.result,
-    i.result_type,
+    insights.result,
+    insights.result_type,
 
-    i.messaging_conversations_started,
-    i.purchase
+    insights.messaging_conversations_started,
+    insights.purchase
 
 from {{ ref('stg_campaign_insights') }} i
 left join `{{ target.project }}.{{ var('company') }}_dataset_google_api_raw.{{ var('company') }}_table_google_{{ var('department') }}_{{ var('account') }}_campaign_metadata` m
-    on i.account_id = m.account_id
-   and i.campaign_id = m.campaign_id
+    on insights.account_id = campaign.account_id
+   and insights.campaign_id = campaign.campaign_id
