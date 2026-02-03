565f44a7964a32b331ea6114f6235789501809b7
nhatquang-nguyen-129 <nhatquang.nguyen.129@gmail.com>
2026-02-03 20:27:18 +0700

    added FacebookAdsApi global client

--- backfill/backfill_facebook_ads.py
+++ backfill/backfill_facebook_ads.py
@@ -3,15 +3,15 @@ import sys
 ROOT_FOLDER_LOCATION = Path(__file__).resolve().parents[1]
 sys.path.append(str(ROOT_FOLDER_LOCATION))
 
+import argparse
 from datetime import datetime
-import json
 import logging
 import os
-import argparse
-from zoneinfo import ZoneInfo
 
 from google.cloud import secretmanager
 from google.api_core.client_options import ClientOptions
+
+from facebook_business.api import FacebookAdsApi
 
 from dags.dags_facebook_ads import dags_facebook_ads
 
@@ -26,18 +26,20 @@ if not all([
     DEPARTMENT,
     ACCOUNT,
 ]):
-    raise EnvironmentError("❌ [TESTS] Failed to execute Facebook Ads manual entrypoint due to missing required environment variables.")
+    raise EnvironmentError("❌ [BACKFILL] Failed to execute Facebook Ads manual entrypoint due to missing required environment variables.")
 
-def main():
+def backfill():
     """
     Backfill Facebook Ads entrypoint
     ---------
-    Workflow
-        1. Parse manual start_date/end_date
-        2. Validate input date range
+    Workflow:
+        1. Get execution time window through argparse
+        2. Validate OS environment variables
         3. Load secrets from GCP Secret Manager
-        4. Initialize global Facebook Ads client
-        5. Dispatch execution to DAGs orchestrator
+        4. Initialize Facebook Ads SDK wrapper exactly once
+        5. Dispatch execution to DAG orchestrator
+    Return:
+        None
     """
 
 # CLI arguments parser for manual date range
@@ -58,13 +60,13 @@ def main():
         start_date = datetime.strptime(args.start_date, "%Y-%m-%d").strftime("%Y-%m-%d")
         end_date = datetime.strptime(args.end_date, "%Y-%m-%d").strftime("%Y-%m-%d")
     except ValueError:
-        raise ValueError("❌ [TESTS] Failed to execute Facebook Ads main entrypoint due to start_date and end_date must be in YYYY-MM-DD format.")
+        raise ValueError("❌ [BACKFILL] Failed to execute Facebook Ads main entrypoint due to start_date and end_date must be in YYYY-MM-DD format.")
 
     if start_date > end_date:
-        raise ValueError("❌ [TESTS] Failed to execute Facebook Ads main entrypoint due to start_date must be less than or equal to end_date.")
+        raise ValueError("❌ [BACKFILL] Failed to execute Facebook Ads main entrypoint due to start_date must be less than or equal to end_date.")
 
     msg = (
-        "🔄 [TESTS] Triggering to execute Facebook Ads main entrypoint for "
+        "🔄 [BACKFILL] Triggering to execute Facebook Ads main entrypoint for "
         f"{ACCOUNT} account of "
         f"{DEPARTMENT} department in "
         f"{COMPANY} company from "
@@ -77,144 +79,128 @@ def main():
 
 # Initialize Google Secret Manager
     try:
-        msg = ("🔍 [TESTS] Initializing Google Secret Manager client...")
+        msg = ("🔍 [BACKFILL] Initialize Google Secret Manager client...")
         print(msg)
         logging.info(msg)
-
+        
         google_secret_client = secretmanager.SecretManagerServiceClient(
             client_options=ClientOptions(
                 api_endpoint="secretmanager.googleapis.com"
             )
         )
 
-        msg = ("✅ [TESTS] Successfully initialized Google Secret Manager client.")
+        msg = ("✅ [BACKFILL] Successfully initialized Google Secret Manager client.")
         print(msg)
         logging.info(msg)
-
+    
     except Exception as e:
         raise RuntimeError(
-            "❌ [TESTS] Failed to initialize Google Secret Manager client due to "
+            "❌ [BACKFILL] Failed to initialize Google Secret Manager client due to."
             f"{e}."
         )
-
-# Resolve customer_id from Google Secret Manager
+        
+# Resolve account_id from Google Secret Manager
     try:
-        secret_customer_id = (
-            f"{COMPANY}_secret_{DEPARTMENT}_google_account_id_{ACCOUNT}"
+        secret_account_id = (
+            f"{COMPANY}_secret_{DEPARTMENT}_facebook_account_id_{ACCOUNT}"
         )
-        secret_customer_name = (
-            f"projects/{PROJECT}/secrets/{secret_customer_id}/versions/latest"
+        secret_account_name = (
+            f"projects/{PROJECT}/secrets/{secret_account_id}/versions/latest"
         )
-
+        
         msg = (
-            "🔍 [TESTS] Retrieving Facebook Ads customer_id from Secret Manager..."
+            "🔍 [BACKFILL] Retrieving Facebook Ads secret_account_id "
+            f"{secret_account_name} from Google Secret Manager..."
         )
         print(msg)
-        logging.info(msg)
+        logging.info(msg)        
 
-        secret_customer_response = google_secret_client.access_secret_version(
-            name=secret_customer_name,
+        secret_account_response = google_secret_client.access_secret_version(
+            name=secret_account_name,
             timeout=10.0,
         )
-
-        google_customer_id = (
-            secret_customer_response.payload.data.decode("utf-8")
-            .replace("-", "")
-            .replace(" ", "")
-            .strip()
-        )
-
+        facebook_account_id = secret_account_response.payload.data.decode("utf-8")
+        
         msg = (
-            "✅ [TESTS] Successfully retrieved Google Ads customer_id "
-            f"{google_customer_id}."
+            "✅ [BACKFILL] Successfully retrieved Facebook Ads customer_id "
+            f"{facebook_account_id} from Google Secret Manager."
         )
         print(msg)
         logging.info(msg)
-
+    
     except Exception as e:
         raise RuntimeError(
-            "❌ [TESTS] Failed to retrieve Google Ads customer_id due to "
+            "❌ [BACKFILL] Failed to retrieve Facebook Ads customer_id from Google Secret Manager due to "
             f"{e}."
         )
 
-# Resolve credentials from Google Secret Manager
     try:
-        secret_credentials_json = (
-            f"{COMPANY}_secret_all_google_token_access_user"
+        secret_token_id = (
+            f"{COMPANY}_secret_all_facebook_token_access_user"
         )
-        secret_credentials_name = (
-            f"projects/{PROJECT}/secrets/{secret_credentials_json}/versions/latest"
+        secret_token_name = (
+            f"projects/{PROJECT}/secrets/{secret_token_id}/versions/latest"
         )
-
+        
         msg = (
-            "🔍 [TESTS] Retrieving Google Ads credentials from Secret Manager..."
+            "🔍 [BACKFILL] Retrieving Facebook Ads secret_credentials_json "
+            f"{secret_token_name} from Google Secret Manager..."
         )
         print(msg)
         logging.info(msg)
 
-        secret_credentials_response = google_secret_client.access_secret_version(
-            name=secret_credentials_name
+        secret_token_response = google_secret_client.access_secret_version(
+            name=secret_token_response
         )
-
-        google_ads_credentials = json.loads(
-            secret_credentials_response.payload.data.decode("UTF-8")
-        )
-
-        msg = (
-            "✅ [TESTS] Successfully retrieved Google Ads credentials."
-        )
+        facebook_token_user = secret_token_response.payload.data.decode("utf-8")
+        
+        msg = ("✅ [BACKFILL] Successfully retrieved Google Ads credentials from Google Secret Manager.")
         print(msg)
         logging.info(msg)
-
+    
     except Exception as e:
         raise RuntimeError(
-            "❌ [TESTS] Failed to retrieve Google Ads credentials due to "
+            "❌ [BACKFILL] Failed to retrieve Google Ads credentials from Google Secret Manager due to "
             f"{e}."
-        )
+        )        
 
 # Initialize global Google Ads client
-    google_ads_config = {
-        "developer_token": google_ads_credentials["developer_token"],
-        "client_id": google_ads_credentials["client_id"],
-        "client_secret": google_ads_credentials["client_secret"],
-        "refresh_token": google_ads_credentials["refresh_token"],
-        "login_customer_id": google_ads_credentials["login_customer_id"],
-        "use_proto_plus": True,
-    }
-
     try:
         msg = (
-            "🔍 [TESTS] Initializing Google Ads client for customer_id "
-            f"{google_customer_id}..."
+            "🔍 [BACKFILL] Initializing global Facebook Ads client for account_id "
+            f"{facebook_account_id}..."
         )
         print(msg)
         logging.info(msg)
 
-        google_ads_client = GoogleAdsClient.load_from_dict(
-            google_ads_config
+        FacebookAdsApi.init(
+            access_token=facebook_token_user, 
+            timeout=180
         )
 
-        msg = ("✅ [TESTS] Successfully initialized Google Ads client.")
+        msg = (
+            "✅ [BACKFILL] Successfully initialized global Facebook Ads client for customer_id "
+            f"{facebook_account_id}."
+        )
         print(msg)
         logging.info(msg)
-
+    
     except Exception as e:
         raise RuntimeError(
-            "❌ [TESTS] Failed to initialize Google Ads client due to "
+            "❌ [BACKFILL] Failed to initialize global Facebook Ads client due to."
             f"{e}."
         )
-
-# Execute DAGs
-    dags_google_ads(
-        google_ads_client=google_ads_client,
-        customer_id=google_customer_id,
+   
+# Execute DAGS
+    dags_facebook_ads(
+        account_id=facebook_account_id,
         start_date=start_date,
-        end_date=end_date,
+        end_date=end_date
     )
 
 # Entrypoint
-if __name__ == "__main__":
+if __name__ == "__backfill__":
     try:
-        main()
+        backfill()
     except Exception:
         sys.exit(1)
