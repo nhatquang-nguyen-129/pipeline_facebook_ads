10b4be18e2bb8b264c249dfbedc3fe2e73547ab7
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-03-30 17:08:10 +0700

    changed token resolver

--- backfill/backfill_campaign_insights.py
+++ backfill/backfill_campaign_insights.py
@@ -43,7 +43,7 @@ def backfill():
         None
     """
 
-# CLI arguments parser for manual date range
+    # CLI arguments parser for manual date range
     parser = argparse.ArgumentParser(
         description="Manual Facebook Ads ETL executor"
     )
@@ -90,8 +90,7 @@ def backfill():
         f"{PROJECT}..."
     )
 
-
-# Initialize Google Secret Manager
+    # Initialize Google Secret Manager
     try:
 
         print(
@@ -115,7 +114,7 @@ def backfill():
             f"{e}."
         )
         
-# Resolve account_id from Google Secret Manager
+    # Resolve account_id from Google Secret Manager
     try:
         
         secret_account_id = (
@@ -150,7 +149,7 @@ def backfill():
             f"{e}."
         )
 
-# Resolve access_token from Google Secret Manager
+    # Resolve access_token from Google Secret Manager
     try:
         
         secret_token_id = (
@@ -182,7 +181,7 @@ def backfill():
             f"{e}."
         )
    
-# Execute DAGS
+    # Execute DAGS
     dags_campaign_insights(
         access_token=access_token,
         account_id=account_id,
@@ -190,7 +189,7 @@ def backfill():
         end_date=end_date
     )
 
-# Entrypoint
+    # Entrypoint
 if __name__ == "__main__":
     
     try:
